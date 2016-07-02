8ccdcbdf9965f52883949c45e7857e84463fbf5b
Sergejs Luhmirins <sluhmirins@mobiquityinc.com>
2016-07-02 15:11:44 +0200

    Initial project commit

---
+++ app/src/main/java/online/luhmirin/visionapiexperiment/PreviewActivity.java
@@ -0,0 +1,14 @@
+package online.luhmirin.visionapiexperiment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PreviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.preview_activity);
+    }
+}
